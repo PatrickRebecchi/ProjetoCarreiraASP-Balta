617949c1088493e612f4bcaf5b97d5243559e68d
Patrick <patrickrebecchi@hotmail.com>
2024-07-03 19:30:24 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
+
+namespace Todo.Controllers;
+
+[ApiController]
+public class HomeController : ControllerBase
+{
+    //Retornar as informações do usuário
+    [HttpGet("/home")]
+    public List<TodoModel> Get([FromServices] AppDbContext context)
+    {
+        return context.Todos.ToList();
+    }
+
+    //Receber as informações do usuário
+    [HttpPost("/")]
+    public TodoModel Post(
+        [FromBody] TodoModel todo,
+        [FromServices] AppDbContext context)
+    {
+        context.Todos.Add(todo);
+        context.SaveChanges();
+
+        return todo;
+    }
+}
